8db188fdff8dcc2783a17cd1bff28bd806041c62
Vladimir Golubev <mrgolubeff@icloud.com>
2024-06-05 22:28:40 +0300

    Squash mobs when jumped on

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 @export var speed = 14
 @export var fall_acceleration = 75
 @export var jump_impulse = 20
+@export var bounce_impulse = 16
 
 var target_velocity = Vector3.ZERO
 
@@ -30,6 +31,19 @@ func _physics_process(delta):
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		target_velocity.y = jump_impulse
+	
+	for index in range(get_slide_collision_count()):
+		var collision = get_slide_collision(index)
+		
+		if collision.get_collider() == null:
+			continue
+		
+		if collision.get_collider().is_in_group("mob"):
+			var mob = collision.get_collider()
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				mob.squash()
+				target_velocity.y = bounce_impulse
+				break
 	
 	velocity = target_velocity
 	move_and_slide()
