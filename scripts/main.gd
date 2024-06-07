3ce3a6b5f1be3f4dd7e8cc71f7a4b1fbcbe12914
Vladimir Golubev <mrgolubeff@icloud.com>
2024-06-07 21:52:15 +0300

    Add ability to restart the game

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,6 +3,10 @@ extends Node
 @export var mob_scene: PackedScene
 
 
+func _ready():
+	$UserInterface/Retry.hide()
+
+
 func _on_mob_timer_timeout():
 	var mob = mob_scene.instantiate()
 	
@@ -19,3 +23,9 @@ func _on_mob_timer_timeout():
 
 func _on_player_hit():
 	$MobTimer.stop()
+	$UserInterface/Retry.show()
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		get_tree().reload_current_scene()
