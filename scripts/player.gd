4c350ca47c6d3b53f7f018ec6f555b441658d6c7
Vladimir Golubev <mrgolubeff@icloud.com>
2024-06-12 17:09:37 +0300

    Animate the player

--- scripts/player.gd
+++ scripts/player.gd
@@ -25,6 +25,9 @@ func _physics_process(delta):
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.basis = Basis.looking_at(direction)
+		$AnimationPlayer.speed_scale = 4
+	else:
+		$AnimationPlayer.speed_scale = 1
 	
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
@@ -49,6 +52,7 @@ func _physics_process(delta):
 	
 	velocity = target_velocity
 	move_and_slide()
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
 
 
 func die():
